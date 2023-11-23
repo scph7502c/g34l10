694eca5dea948379bceb3e9ecff97591a6dac395
Damian Temimi <damian.temimi@gmail.com>
2023-11-23 20:44:10 +0100

    Created MessagesFileCreator.java

---
+++ src/main/java/task0/MessagesFileCreator.java
@@ -0,0 +1,41 @@
+package task0;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class MessagesFileCreator {
+    public static void main(String[] args) {
+        String[] lines = {
+                "Kurs",
+                "Java",
+                "Lekcja 6",
+                "Pliki",
+                "Wyjątki",
+                "Pliki",
+                "Koniec pliku"
+        };
+
+        String filePath = "C:\\Users\\damia\\OneDrive\\Pulpit\\FutureCollars\\Java\\Projekty\\g34l10\\src\\main\\resources\\data.txt";
+
+        Path path = Paths.get(filePath);
+
+        try {
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+                System.out.println("Plik " + path.getFileName() + " został pomyślnie utworzony.");
+            }
+
+            List<String> content = Files.readAllLines(path);
+            for (String line : lines) {
+                content.add(line);
+            }
+            Files.write(path, content);
+            System.out.println("Zawartość została pomyślnie dodana do pliku " + path.getFileName());
+        } catch (IOException e) {
+            System.err.println("Wystąpił błąd podczas aktualizacji pliku: " + e.getMessage());
+        }
+    }
+}
